91bc63a7ae43bbf2ba7d2ed772ee7fba13e364e9
gabrielking1 <gkgabrielking@gmail.com>
2023-07-14 14:17:04 +0100

    {newly pushed}

--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -16,11 +16,14 @@ Including another URLconf
 from django.contrib import admin
 
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('Port/', admin.site.urls),
     path('', include('Myapp.urls')),
    
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
